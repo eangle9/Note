8550a90d2eee23233ede94bd861d930e7416cc8b
eangle9 <engdaworkyismaw9@gmail.com>
2024-06-04 14:50:01 +0300

    chore: add vercel configuration

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,13 @@ const app = express();
 dotenv.config();
 ConnectDb();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://note-zipper-three.vercel.app/"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // app.get("/users", async(req, res) => {
 //   const users = await User.find();
